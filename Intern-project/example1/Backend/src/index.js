b2b07015ca68be59e994bce52b02ee45a587cb03
Souhail1337 <souhailfcht@gmail.com>
2023-06-08 16:02:43 +0100

    only mongodb left

--- Intern-project/example1/Backend/src/index.js
+++ Intern-project/example1/Backend/src/index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = 4000;
 
+mongoose.connect('localhost:27017',{ 
+    useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log("EEEEEE CONNECTED TO MONGODB");
+    })
+    .catch((error) =>{
+        console.log('Failed to connect to mongo: ', error);
+    });
+const exSchema = new mongoose.Schema({
+    message: String,
+    nuum: Number,
+});
+
+const exModel = mongoose.model('Example: ',exSchema);
 
 app.get('/', (req, res) => {
     res.send('Hello Test From backend !');
@@ -14,6 +29,16 @@ app.get('/Test', (req, res) =>
     res.send('Redirected to test!');
 });
 
+
+app.get('/examples', async (req,res)=>{
+    try{
+        const examples = await exModel.find();
+        res.json(examples);
+    } catch(error){
+        console.error('Error retrieving exapmle ',error);
+        res.status(500).json({error: 'an error occured'});
+    }
+});
 app.listen(port,() =>
 {
     console.log('Listening on Port 4000');
