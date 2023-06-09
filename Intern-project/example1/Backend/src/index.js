06bb96ca5e7523ddf6e815bc1d87d460f0bb9935
Souhail1337 <souhailfcht@gmail.com>
2023-06-09 14:15:15 +0100

    almost finished all containers for example 1

--- Intern-project/example1/Backend/src/index.js
+++ Intern-project/example1/Backend/src/index.js
@@ -1,27 +1,90 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const User = require("./models/user");
+var passport = require("passport");
+var bodyParser = require('body-parser');
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
 const port = 4000;
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
 
-mongoose.connect('mongodb://db_container:27017/my_db',{ 
-    useNewUrlParser: true, useUnifiedTopology: true})
+
+mongoose.connect('mongodb+srv://souhail:souhail@cluster0.0p01gnu.mongodb.net/',options)
     .then(() => {
-        console.log("EEEEEE CONNECTED TO MONGODB");
+        console.log("CONNECTED TO MONGODB");
     })
     .catch((error) =>{
         console.log('Failed hhhh to connect to mongo: ', error);
     });
-const exSchema = new mongoose.Schema({
+    app.set('view engine','ejs');
+    app.engine('html',require('ejs').renderFile);
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    const exSchema = new mongoose.Schema({
     message: String,
     nuum: Number,
 });
 
 const exModel = mongoose.model('Example: ',exSchema);
 
+app.use(require("express-session")({
+    secret: "test a lot of tests",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+})
+
+
 app.get('/', (req, res) => {
     res.send('Hello Test From backend !');
 });
 
+app.get("/register", (req,res) => res.render("register"));
+app.post("/register", (req, res) => {
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}) ,
+    req.body.password, function(err, user){
+        if (err)
+        {
+            console.log("testt " + err);
+        }
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/");
+        });
+    });
+});
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",{
+       successRedirect:"/success",
+       failureRedirect:"/register" }), 
+       function(req,res){      
+
+
+});
+app.get('/success', (req, res) =>
+{
+    res.send('Successfully registred and logged in !');
+});
 
 
 app.get('/Test', (req, res) =>
